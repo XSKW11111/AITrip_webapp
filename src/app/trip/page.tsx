03421d7692986bad5bcb88994e15d41391becb74
XSKW11111 <39919931+XSKW11111@users.noreply.github.com>
2024-12-13 10:28:22 -0800

    feat(aitrip): remove npm test in pre-commit hook

--- src/app/trip/page.tsx
+++ src/app/trip/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Input, VStack, HStack, Box, Button, Center } from "@chakra-ui/react";
 import React, { useState } from "react";
@@ -8,60 +8,57 @@ import { Trip } from "@/type/trip_base";
 import { IoSend } from "react-icons/io5";
 
 const TripPage = (): React.ReactElement => {
-    const [inputContent, setInputContent] = useState('');
-    const [travelData, setTravelData] = useState<Trip[]>([]);
+  const [inputContent, setInputContent] = useState("");
+  const [travelData, setTravelData] = useState<Trip[]>([]);
 
-    const getOpenAIResult = async () => {
-        let res = [];
-        try {
-            res = await getTripPlanFromAI(inputContent);
-            setTravelData(res.travelPlan as Trip[]);
-        }
-        catch(e) {
-            console.log(JSON.stringify(e));
-        }
-    };
-
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newInputContent = e.target.value;
-        setInputContent(newInputContent);
+  const getOpenAIResult = async () => {
+    let res = [];
+    try {
+      res = await getTripPlanFromAI(inputContent);
+      setTravelData(res.travelPlan as Trip[]);
+    } catch (e) {
+      console.log(JSON.stringify(e));
     }
+  };
 
-    const handleSend = () => {
-        void getOpenAIResult();
-    }
-    return (
-        <VStack padding={20} gap='40px'>
-            <HStack w='100%' justifyContent='center'>
-                <Input maxWidth={600} value={inputContent} onChange={handleInputChange}/>
-                <Button w='40px' h='40px' backgroundColor='teal.400' borderRadius={8} onClick={handleSend}>
-                    <Center>
-                        <IoSend size={30} />
-                        </Center>
-                    </Button>
-            </HStack>
-            <Box
-                overflowX='auto'
-                width='100%'
-                paddingBottom='20px'
-            >
-                <HStack 
-                    width='fit-content'
-                    gap='60px'
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newInputContent = e.target.value;
+    setInputContent(newInputContent);
+  };
 
-                >
-                {travelData && travelData.map((trip, index) => {
-                    return (
-                        <TripCard key={index} trip={trip} index={index}/>
-                    )
-                })
-                
-                }
-                </HStack>
-            </Box>
-        </VStack>
-    )
+  const handleSend = () => {
+    void getOpenAIResult();
+  };
+  return (
+    <VStack padding={20} gap="40px">
+      <HStack w="100%" justifyContent="center">
+        <Input
+          maxWidth={600}
+          value={inputContent}
+          onChange={handleInputChange}
+        />
+        <Button
+          w="40px"
+          h="40px"
+          backgroundColor="teal.400"
+          borderRadius={8}
+          onClick={handleSend}
+        >
+          <Center>
+            <IoSend size={30} />
+          </Center>
+        </Button>
+      </HStack>
+      <Box overflowX="auto" width="100%" paddingBottom="20px">
+        <HStack width="fit-content" gap="60px">
+          {travelData &&
+            travelData.map((trip, index) => {
+              return <TripCard key={index} trip={trip} index={index} />;
+            })}
+        </HStack>
+      </Box>
+    </VStack>
+  );
 };
 
 export default TripPage;
